3307e73e853b2b849164f40d2418b7f8573f46e5
sagarinitcron <sagar@initcron.org>
2016-08-25 15:26:04 +0530

    Update mongodbsecset.pp

--- modules/roles/manifests/mongodbsecset.pp
+++ modules/roles/manifests/mongodbsecset.pp
@@ -3,7 +3,10 @@ class {'::mongodb::globals':
   manage_package_repo => true,
 }->
 class {'::mongodb::server':
+bind_ip    => ['0.0.0.0'],
+smallfiles => true,
+replset    => 'rsmain',
  }->
-class {'::mongodb::client': 
+class {'::mongodb::client':
 }
 }
